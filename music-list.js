0b590747ae721d33ffa077b7b78e83bdb1e8ff44
Alex99like <aleksievisa@gmail.com>
2022-02-06 23:25:23 +0300

    create: audio

---
+++ music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+    {
+      name: "Lemonade",
+      artist: "Beyonce",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Down start Now",
+      artist: "Dony",
+      img: "music-2",
+      src: "music-2"
+    }
+]
